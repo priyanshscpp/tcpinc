ca628450e8a2921f30fc5a17c0d4ac96ff1fcc7e
priyanshscpp <priyanshusgvm@gmail.com>
2025-07-22 01:46:15 +0530

    Bengali

--- CSES/Minimizing_Coins.cpp
+++ CSES/Minimizing_Coins.cpp
@@ -8,9 +8,17 @@ const int INF=0x3f3f3f3f;
 int N,x,c ,dp[maxx+1];
 
 int main (){
-   cin>>x;
+   cin>>N>>x;
    fill(dp+1, dp+x+1,INF);
-   
+   for(int i=0; i<N; i++){
+    cin>>c;
+    for(int j=0; j<=x-c; j++)
+    if(dp[j]!=INF)
+      dp[j+c]=min(dp[j+c],dp[j]+1);
+
+   }
+   cout<<(dp[x]==INF ?-1:dp[x])<<endl;
+
 
     return 0;
 
