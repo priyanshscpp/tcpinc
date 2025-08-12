7a0a3a722e4f6126d1149f7e69d9caff9c90adea
priyanshscpp <priyanshusgvm@gmail.com>
2025-08-13 01:14:43 +0530

    man

--- F_Maximum_White_Subtree.cpp
+++ F_Maximum_White_Subtree.cpp
@@ -39,12 +39,12 @@ ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
 string to_upper(string a) { for (int i = 0; i < (int)a.size(); ++i) if (a[i] >= 'a' && a[i] <= 'z') a[i] -= 'a' - 'A'; return a; }
 string to_lower(string a) { for (int i = 0; i < (int)a.size(); ++i) if (a[i] >= 'A' && a[i] <= 'Z') a[i] += 'a' - 'A'; return a; }
 bool prime(ll a) { if (a == 1) return 0; for (int i = 2; i <= round(sqrt(a)); ++i) if (a % i == 0) return 0; return 1; }
-void yes() { cout << "YES\n"; }
+void yes() { cout << \n"; }
 void no() { cout << "NO\n"; }
 
 /* clang-format on */
 const int N=2e5+10;
-vector<int> g[N];
+// vector<int> g[N];
 bool c[N];
 int n,dp[N],ans[N];
 
@@ -86,7 +86,7 @@ int main()
   dfs(0,0);
   for(int i=0; i<n; i++)
   cout<<ans[i]<<" ";
-  
+
 
     return 0;
 }
